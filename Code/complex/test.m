d41a2ef4eedabbbe4f56d5cfed251249d3038d23
Zhihao Jiang <zhihaoj@seas.upenn.edu>
2014-03-21 21:51:31 -0400

    tuning

--- Code/complex/test.m
+++ Code/complex/test.m
@@ -5,21 +5,18 @@ load case3_AVNRT
  data=[];
  while clk<3000
      clk=clk+1;
-     if clk==1 || mod(clk,380)==0%==1 || clk==600 || clk==940
+     if clk==1 || mod(clk,250)==0%==1 || clk==600 || clk==940
          node_table{1,10}=1;
      end
-     
-  if clk==939
-      1
-  end
+     data=[data,[node_table{1,10},node_table{2,10},node_table{4,10},node_table{7,10},node_table{3,4}]'];
     [node_table,path_table]=heart_model(node_table,path_table); 
      
-    data=[data,[node_table{2,10},node_table{4,10},node_table{7,10},node_table{3,4}]'];
+    
      
  end
  
  figure
  axes('nextplot','add');
- for i=1:3%size(data,1)
+ for i=1:4%size(data,1)
      plot(data(i,:)-i*1.5);
  end
