c0ad66ffbad37f8fc4b5d90d8b7db91a45495a41
Zhihao Jiang <zhihaoj@seas.upenn.edu>
2014-04-09 20:09:49 -0400

    first version monitor

--- Code/complex/monitor.m
+++ Code/complex/monitor.m
@@ -1,5 +1,15 @@
 function [pace,condition]=monitor(S1,A1,A2,A6,A7)
-
+global m_state
+global Tpace
+global t
+global lastSA
+global lastAH
+global lastHV
+global Intrinsic
+global slowpath
+global AVNRT
+global terminated
+global pace_round
 condition=0;
 pace=0;
 switch m_state
@@ -22,7 +32,7 @@ switch m_state
     case 'waitI6'
         if A6
             m_state='waitI7';
-            lastAV=t;
+            lastAH=t;
             t=0;
         end
         return
@@ -39,5 +49,77 @@ switch m_state
         end
         return
     case 'InitPace'
+        if A2 && slowpath==1
+            AVNRT=1;
+            m_state='AVNRT2';
+        else if Tpace>=S1
+                m_state='wait2';
+                pace=1;
+                pace_round=pace_round+1;
+                Tpace=0;
+                t=0;
+            end
+        end
+        return
+    case 'wait2'
+        if Tpace>=S1
+            condition=1;
+            pace=1;
+            terminated=1;
+            return
+        end
+        if A2
+            m_state='wait6';
+            if abs(t-lastSA)>=2
+                condition=2;
+            end
+        end
+        return
+    case 'wait6'
+        if Tpace>=S1
+            m_state='paceb4A6';
+            pace=1;
+            pace_round=pace_round+1;
+            Tpace=0;
+            return
+        end
+        if A6
+            m_state='wait7';
+            slowpath=0;
+            if abs(t-lastAH)>5
+                condition=5;
+            else if pace_round==1
+                    condition=6;
+                else
+                    condition=7;
+                end
+            end
+        end
+            return
+    case 'paceb4A6'
+        if Tpace>=100
+            condition=3;
+            terminated=1;
+            return
+        end
+        if A6
+            condition=4;
+            slowpath=1;
+            m_state='wait7';
+        end
+        return
+    case 'wait7'
+        if pace_round>=10
+            condition=8;
+            terminated=1;
+            return
+        end
+        if A7
+            lastHV=t;
+            m_state='InitPace';
+        end
+        return
+end
+        
         
             
--- Code/complex/test.m
+++ Code/complex/test.m
@@ -1,9 +1,17 @@
-clear all
+% clear all
 close all
 load case3_AVNRT
 global Tpace
 global t
 global m_state
+global lastSA
+global lastAH
+global lastHV
+global Intrinsic
+global slowpath
+global AVNRT
+global terminated
+global pace_round
 % loading parameters of virtual heart model (VHM)
  clk=0;
  % global clock (one iteration represents 1 ms)
@@ -15,24 +23,40 @@ global m_state
  Tpace=0;
  t=0;
  m_state='waitI1';
- while clk<3000
+ lastSA=0;
+ lastAH=0;
+ lastHV=0;
+ Intrinsic=0;
+ slowpath=0;
+ AVNRT=0;
+ terminated=0;
+ pace_round=0;
+ 
+ S1=350;
+ while clk<10000
      % run this algorithm for 3000 iterations (i.e. 3 seconds)
      clk=clk+1;
      Tpace=Tpace+1;
      t=t+1;
-%      if clk==1 || mod(clk,380)==0 %==1 || clk==600 || clk==940
-%          node_table{1,10}=1;
-%          % activate node one (SA node) on the first iteration and every 380 ms thereafter
-%      end
-     data=[data,[node_table{1,10},node_table{2,10},node_table{4,10},node_table{7,10},node_table{3,4}]'];
-     % importing the activation states of the nodes for which there are
-     % probes
-    [node_table,path_table]=heart_model(node_table,path_table);
-    % running heart model for one iteration (and outputting result back
-    % into node_table and path_table)
      
     
-     
+   
+    if clk==1
+        node_table{1,10}=1;
+    end
+    if ~terminated
+         [pace,condition]=monitor(S1,node_table{1,11},node_table{2,10},node_table{6,10},node_table{7,10});
+    else
+        break;
+    end
+    
+     if pace
+        node_table{1,10}=1;
+    end
+    data=[data,[node_table{1,11},node_table{2,10},node_table{4,10},node_table{7,10},node_table{3,4}]'];
+
+    [node_table,path_table]=heart_model(node_table,path_table);
+    
  end
  
  figure
