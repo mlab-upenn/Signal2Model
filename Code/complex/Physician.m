a3f4cd0729cbdb202bb713be23d435d0b4bd2b44
Hao <zhihaoj@seas.upenn.edu>
2014-04-29 17:57:28 -0400

    egm initial attempt

---
+++ Code/complex/Physician.m
@@ -0,0 +1,108 @@
+%clear all
+close all
+load test
+global Tpace
+global t
+global m_state
+global lastSA
+global lastAH
+global lastHV
+global Intrinsic
+global slowpath
+global AVNRT
+global terminated
+global pace_round
+global A_slowed
+global AV_slowed
+
+% loading parameters of virtual heart model (VHM)
+ clk=0;
+ % global clock (one iteration represents 1 ms)
+ data=[];
+ % virtual electrogram that we want to print (matrix containing activation
+ % state of each node
+ % 1 = activated
+ % 0 = inactivated
+ Tpace=0;
+ t=0;
+ m_state='waitI1';
+ lastSA=0;
+ lastAH=0;
+ lastHV=0;
+ Intrinsic=0;
+ slowpath=0;
+ AVNRT=0;
+ terminated=0;
+ pace_round=0;
+ A_slowed=0;
+ AV_slowed=0;
+ 
+ S1=340;
+
+ const=[];
+ for S1=340%300:10:460
+     load test
+      cond=[];
+      clk=0;
+     Tpace=0;
+ t=0;
+ m_state='waitI1';
+ lastSA=0;
+ lastAH=0;
+ lastHV=0;
+ Intrinsic=0;
+ slowpath=0;
+ AVNRT=0;
+ terminated=0;
+ pace_round=0;
+ A_slowed=0;
+ AV_slowed=0;
+ while clk<10000
+     % run this algorithm for 3000 iterations (i.e. 3 seconds)
+     clk=clk+1;
+     Tpace=Tpace+1;
+     t=t+1;
+     
+%     if clk==3560
+%         1
+%     end
+   
+    if clk==1
+        node_table{1,10}=1;
+    end
+    if ~terminated
+         [pace,condition]=monitor(S1,node_table{1,11},node_table{2,10},node_table{6,10},node_table{7,10});
+         if ~strcmp(condition,'R0')
+             cond=[cond;{clk,condition}];
+             sing_const=constraint(condition,S1);
+             const=[const;sing_const];
+         end
+    else
+        %break;
+    end
+    
+     if pace
+        node_table{1,10}=1;
+    end
+    data=[data,[node_table{1,11},node_table{2,10},node_table{4,10},node_table{6,10},node_table{7,10},node_table{3,4}]'];
+
+    [node_table,path_table]=heart_model(node_table,path_table);
+    
+ end
+ end
+ template=[0.5,1,0.5,0,-0.5,-1,-0.5];
+ egm=zeros(5,size(data,2));
+ for i=1:5
+    temp=find(data(1,:)==1);
+    for j=1:length(temp)
+        egm(i,temp(j):temp(j)+6)=template;
+    end
+   
+ end
+ egm(2,:)=0.8.*egm(2,:)+0.5.*egm(3,:)+egm(4,:);
+ egm(3:4,:)=[];
+ figure
+ axes('nextplot','add');
+ for i=1:4%size(data,1)
+     plot(egm(i,:)-i*1.5);
+ end
